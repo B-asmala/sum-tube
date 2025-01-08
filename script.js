37cc908e319d4d66058d3e1e5486627b278a41de
B-asmala <basmalabayoumi@gmail.com>
2025-01-08 23:55:37 +0200

    implemented waking server function

---
+++ script.js
@@ -0,0 +1,28 @@
+const apiURL = 'http://127.0.0.1:3000/';
+
+//wake up the server(hosted on glitch and put to sleep by default)
+//if successful change the UI to accept user input
+checkAwake();
+
+async function checkAwake (){
+    const wakeUpResponse = await (await fetch(apiURL + 'wake')).json();
+
+    if(wakeUpResponse.message == 'awake'){//server woke up, change ui to accept input
+        changeUI();
+    }else{
+        alert("problem with server, try again later");
+    }
+}
+
+
+
+
+function changeUI(){
+    //change header text
+    const h1 = document.getElementById('header');
+    h1.innerHTML = "Enter video URL";
+
+    //show the form
+    const form = document.getElementById('form');
+    form.classList.remove('hidden');
+}
