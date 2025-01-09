a97ccd8218dca65b451ae4ad1068a5d74e75163d
B-asmala <basmalabayoumi@gmail.com>
2025-01-10 00:09:16 +0200

    handle form submission

--- script.js
+++ script.js
@@ -4,6 +4,51 @@ const apiURL = 'http://127.0.0.1:3000/';
 //if successful change the UI to accept user input
 checkAwake();
 
+document.getElementById('form').onsubmit = async (event) => {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+
+    //remove red border from last invalid input
+    const textInput = document.getElementById('text-input');
+    textInput.classList.remove('red-border');
+    textInput.classList.add('colorful-border');
+
+    const div = document.getElementById('sum-container');
+    div.innerHTML = "...";
+    try {
+        let URL = document.getElementById('text-input').value;
+
+        //check if input is valid
+        if(URL.includes('youtu.be') || URL.includes('youtube')){ 
+            if(URL.includes('youtu.be')){
+                //https://youtu.be/iiiiiiiiiii
+                URL = URL.slice(17, 28);
+    
+            }else if(URL.includes('youtube')){
+                //https://www.youtube.com/watch?v=iiiiiiiiiii
+                URL = URL.slice(32, 43);
+            }
+
+
+            //make API call
+            const subs = await (await fetch(apiURL + 'sum/' + URL)).json();
+            //display in div
+            div.innerHTML = subs.data;
+        }else{//invalid input
+
+            textInput.classList.remove('colorful-border');
+            textInput.classList.add('red-border');
+            alert("Invalid Input");
+
+        }
+        
+    } catch (err) {
+        console.log(err);
+        alert("Error with server, try again later");
+    }
+    
+}
+
+
 async function checkAwake (){
     try{
         const wakeUpResponse = await (await fetch(apiURL + 'wake')).json();
